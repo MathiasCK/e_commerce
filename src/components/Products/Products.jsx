87c512ba2b366d9de0e1aee4dd2ff702364b61fc
Mathias Kolberg <mathias.ck2000@gmail.com>
2021-04-21 22:07:39 +0200

    Adding Products

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,38 @@
+import { Grid } from "@material-ui/core";
+import React from "react";
+import Product from "./Product/Product";
+
+const products = [
+  {
+    id: 1,
+    name: "Rhoes",
+    description: "Running shoes",
+    price: "50$",
+    image:
+      "https://cdn.runrepeat.com/i/balenciaga/31210/balenciaga-triple-s-clear-sole-trainers-44ba-600.jpg",
+  },
+  {
+    id: 2,
+    name: "Macbook",
+    description: "Apple macbook",
+    price: "500$",
+    image:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-pro-13-og-202011?wid=600&hei=315&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1604347427000",
+  },
+];
+
+const Products = () => {
+  return (
+    <main>
+      <Grid container justify="center" spacing="4">
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+};
+
+export default Products;
