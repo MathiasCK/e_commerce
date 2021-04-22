b94ce3d0b3b8eab983c240dfb9eed5e8d133b436
Mathias Kolberg <mathias.ck2000@gmail.com>
2021-04-22 15:45:00 +0200

    Fix

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // Components
 import { Products, Navbar, Cart } from "./components";
-import { useCart, useFetchCart } from "./context/CartContext";
+import { useFetchCart } from "./context/CartContext";
 import { useFetchProducts } from "./context/ProductsContext";
 
 const App = () => {
   const fetchProducts = useFetchProducts();
-
-  const cart = useCart();
   const fetchCart = useFetchCart();
 
   useEffect(() => {
@@ -19,17 +17,15 @@ const App = () => {
 
   return (
     <Router>
-      <div>
-        <Navbar totalItems={cart.total_items} />
-        <Switch>
-          <Route exact path="/cart">
-            <Cart />
-          </Route>
-          <Route exact path="/">
-            <Products />
-          </Route>
-        </Switch>
-      </div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="/">
+          <Products />
+        </Route>
+      </Switch>
     </Router>
   );
 };
